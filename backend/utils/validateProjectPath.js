e5916d070bb4d84520d28cea23d881826a006974
DY-2001 <dushyant.y20@iiits.in>
2026-07-02 02:04:17 +0530

    fixed dockerfile name

--- backend/utils/validateProjectPath.js
+++ backend/utils/validateProjectPath.js
@@ -2,19 +2,23 @@ const fs = require("fs");
 const path = require("path");
 
 function validateProjectPath(clonePath, dockerfilePath) {
-  const projectPath = path.join(clonePath, dockerfilePath);
+    const projectPath = path.join(clonePath, dockerfilePath);
 
-  if (!fs.existsSync(projectPath)) {
-    throw new Error("Invalid Dockerfile path.");
-  }
+    if (!fs.existsSync(projectPath)) {
+        throw new Error("Invalid Dockerfile path.");
+    }
 
-  const dockerfileFullPath = path.join(projectPath, "Dockerfile");
+    const files = fs.readdirSync(projectPath);
 
-  if (!fs.existsSync(dockerfileFullPath)) {
-    throw new Error("Dockerfile not found.");
-  }
+    const dockerfile = files.find(
+        file => file.toLowerCase() === "dockerfile"
+    );
 
-  return projectPath;
+    if (!dockerfile) {
+        throw new Error("Dockerfile not found.");
+    }
+
+    return projectPath;
 }
 
 module.exports = validateProjectPath;
