30e0d275ed35ea35dd7517674ab19248a21be904
DY-2001 <dushyant.y20@iiits.in>
2026-07-02 12:50:11 +0530

    change to duckdns url

--- backend/utils/getTunnelUrl.js
+++ backend/utils/getTunnelUrl.js
@@ -1,13 +1,5 @@
-const fs = require("fs");
-
 function getTunnelUrl() {
-  const filePath = "/app/cloudflare-url.txt";
-
-  if (!fs.existsSync(filePath)) {
-    throw new Error("Cloudflare Tunnel URL not found.");
-  }
-
-  return fs.readFileSync(filePath, "utf8").trim();
+    return "http://mhfs.duckdns.org";
 }
 
 module.exports = getTunnelUrl;
